4e3790f0acd312f7575529424aab836ec34e839e
ydshiroma <ydshiroma@gmail.com>
2025-07-16 12:49:09 -0400

    search uses both __search and __icontains

--- meetingsvideos/service.py
+++ meetingsvideos/service.py
@@ -6,17 +6,24 @@ from .models import Video, Speaker, LCSH
 # searches title, abstract, speaker, and lcsh for search term
 #TODO: fix this - using SearchVector gives a ton of duplicate results that aren't removed by duplicate(), issue seems to be searching on many-to-many field
 def basic_search(query):
-    video_vector = SearchVector('title', 'abstract', 'lcsh__heading')
+    # video_vector = SearchVector('title', 'abstract', 'lcsh__heading')
+    video_search = Q(title__search=query) | Q(title__search=query) | Q(abstract__search=query) | Q(speakers__display_name__search=query) | Q(speakers__lcsh__heading__search=query) | Q(lcsh__heading__search=query)
+    
+    video_icontains = Q(title__icontains=query) | Q(title__icontains=query) | Q(abstract__icontains=query) | Q(speakers__display_name__icontains=query) | Q(speakers__lcsh__heading__icontains=query) | Q(lcsh__heading__icontains=query)
 
     # videos = Video.objects.annotate(search=video_vector).filter(search=query).distinct()
-    videos = Video.objects.filter(Q(title__search=query) | Q(abstract__search=query) | Q(speakers__display_name__search=query) | Q(speakers__lcsh__heading__search=query) | Q(lcsh__heading__search=query)).distinct()
+    videos = Video.objects.filter(video_search | video_icontains).distinct()
     
     # speaker_vector = SearchVector('display_name', weight='A') + SearchVector('lcsh__heading', weight='B') + SearchVector('affiliation__position', 'affiliation__institution', weight='C')
-    speakers = Speaker.objects.filter(Q(display_name__search=query) | Q(lcsh__heading__search=query) | Q(affiliation__position__search=query) | Q(affiliation__institution__search=query)).distinct()
+    speaker_search = Q(display_name__search=query) | Q(lcsh__heading__search=query) | Q(affiliation__position__search=query) | Q(affiliation__institution__search=query)
+    
+    speaker_icontains = Q(display_name__icontains=query) | Q(lcsh__heading__icontains=query) | Q(affiliation__position__icontains=query) | Q(affiliation__institution__icontains=query)
+    
+    speakers = Speaker.objects.filter(speaker_search | speaker_icontains).distinct()
     
     # search LCSH, excluding any associated only with speakers
-    subject_vector = SearchVector('heading')
-    # subjects = LCSH.objects.filter(heading__search=query).exclude(video=None)
-    subjects = LCSH.objects.annotate(search=subject_vector).filter(search=query).exclude(video=None)
+    # subject_vector = SearchVector('heading')
+    # subjects = LCSH.objects.annotate(search=subject_vector).filter(search=query).exclude(video=None)
+    subjects = LCSH.objects.filter(Q(heading__search=query) | Q(heading__icontains=query)).exclude(video=None)
     
     return videos, speakers, subjects
