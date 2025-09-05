153942e044c313f64dd276ceb35e878f2a23829f
David Ragnar Nelson <drnelson@gmail.com>
2025-09-05 11:50:26 -0400

    Add CSRF_TRUSTED_ORIGINS to settings

--- video-site/mysite/settings.py
+++ video-site/mysite/settings.py
@@ -30,6 +30,7 @@ SECRET_KEY = os.getenv("DB_SECRET_KEY")
 DEBUG = bool(int(os.getenv("DEBUG")))
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS").split(" ")
+CSRF_TRUSTED_ORIGINS = os.getenv("DJANGO_TRUSTED_ORIGINS").split(" ")
 
 
 # Application definition
