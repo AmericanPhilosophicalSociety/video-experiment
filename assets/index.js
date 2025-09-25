78316d3f320616d139568cb0aec7086dfe87c0c8
David Ragnar Nelson <drnelson@gmail.com>
2025-09-25 15:46:51 -0400

    Deprecate HTMx nav function and add local function

--- assets/index.js
+++ assets/index.js
@@ -3,26 +3,17 @@ import htmx from 'htmx.org/dist/htmx.esm';
 
 window.htmx = htmx;
 
-window.onload = function() {
-    const navItems = document.querySelectorAll(".main-nav-item");
-    navItems.forEach(d => d.addEventListener('click', selectNav))
-
-    function selectNav(evt) {
-      let currentTab = document.querySelector('[aria-current=page]');
-      currentTab.classList.remove('active')
-      currentTab.removeAttribute('aria-current')
-      let newTab = evt.target
-      newTab.setAttribute('aria-current', 'page')
-      newTab.classList.add('active')
-  }
-};
+export function makeActive(ele) {
+  const navLink = document.getElementById(ele);
+  navLink.setAttribute('aria-current', 'page');
+  navLink.classList.add('active');
+}
 
 htmx.onLoad(function(content) {
     const navTabs = content.querySelectorAll(".nav-tab-link")
     navTabs.forEach(d => d.addEventListener('click', selectTab))
 
     function selectTab(evt) {
-      console.log("hello world!")
       let currentTab = document.querySelector('[aria-selected=true]');
       currentTab.setAttribute('aria-selected', 'false');
       currentTab.classList.remove('active');
